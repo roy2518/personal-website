d5351ee693afdf6723685c8cb5fbb0dc9d24b20d
Roy Xiong <roy.xiong.1@gmail.com>
2021-12-16 01:09:30 -0600

    Add experience

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -71,6 +71,32 @@ const Home = (): JSX.Element => (
       >
         <div className="sectionHeading">
           <h1>Experience</h1>
+          <div>
+            <div className="dateAndLocation">
+              May 2021 - Aug 2021 | Pittsburgh, PA (remote)
+            </div>
+            <div className="position">
+              Software Engineering Intern
+              {' @ '}
+              <a href="https://duolingo.com" rel="noreferrer" target="_blank">Duolingo</a>
+            </div>
+            <div className="dateAndLocation">
+              May 2020 - Aug 2020 | Austin, TX (remote)
+            </div>
+            <div className="position">
+              Software Engineering Intern
+              {' @ '}
+              <a href="https://amazon.com" rel="noreferrer" target="_blank">Amazon</a>
+            </div>
+            <div className="dateAndLocation">
+              May 2019 - Aug 2019 | Durham, NC
+            </div>
+            <div className="position">
+              TDP Intern
+              {' @ '}
+              <a href="https://optum.com" rel="noreferrer" target="_blank">Optum</a>
+            </div>
+          </div>
         </div>
       </Plx>
     </div>
