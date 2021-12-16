2097c93817c6c00cca38a4dc961150aa5a1ca053
Roy Xiong <roy.xiong.1@gmail.com>
2021-12-16 15:51:20 -0600

    Make linter happy and add back animation

--- src/components/home/ProjectsSection.tsx
+++ src/components/home/ProjectsSection.tsx
@@ -3,14 +3,30 @@ import Plx from 'react-plx';
 
 import styles from 'styles/home/ProjectsSection.module.scss';
 
-const ProjectsSection = (): JSX.Element => {
-  return (
-    <div className={styles.container}>
-        <div className={styles.sectionHeading}>
-          <h2>Projects</h2>
-        </div>
-    </div>
-  );
-};
+const ProjectsSection = (): JSX.Element => (
+  <div className={styles.container}>
+    <Plx
+      parallaxData={[{
+        start: 'self',
+        duration: '100vh',
+        properties: [{
+          startValue: 0,
+          endValue: 1,
+          property: 'opacity',
+        },
+        {
+          startValue: -30,
+          endValue: 0,
+          unit: 'vh',
+          property: 'translateY',
+        }],
+      }]}
+    >
+      <div className={styles.sectionHeading}>
+        <h2>Projects</h2>
+      </div>
+    </Plx>
+  </div>
+);
 
 export default ProjectsSection;
