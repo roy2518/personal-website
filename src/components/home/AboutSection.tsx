1735ee0fc2ac39a7b59ca52d3176c086fdb274f4
Roy Xiong <roy.xiong.1@gmail.com>
2021-12-16 12:05:42 -0600

    Refactor about section

---
+++ src/components/home/AboutSection.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Plx from 'react-plx';
+
+import Header from 'components/common/Header';
+import ScrollArrow from 'components/common/ScrollArrow';
+import WordCycler from 'components/home/WordCycler';
+
+import styles from 'styles/home/AboutSection.module.scss';
+
+import { Page } from 'utils/types';
+
+const AboutSection = (): JSX.Element => (
+  <div>
+    <Plx
+      parallaxData={
+        [
+          {
+            start: 'self',
+            duration: '50vh',
+            properties: [
+              {
+                startValue: 1,
+                endValue: 0,
+                property: 'opacity',
+              },
+            ],
+          },
+        ]
+      }
+    >
+      <div className={styles.container}>
+        <Header page={Page.About} />
+        <div className={styles.aboutText}>
+          <h2>Hi there! I&apos;m Roy, a</h2>
+          <div className={styles.descriptor}>
+            <WordCycler words={['student', 'developer', 'musician']} />
+          </div>
+          <p>currently attending Duke University.</p>
+          <p>I&apos;ll be graduating in Spring 2022 with a B.S. in Computer Science!</p>
+        </div>
+        <ScrollArrow />
+      </div>
+    </Plx>
+  </div>
+);
+
+export default AboutSection;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,47 +1,14 @@
 import React from 'react';
 import Plx from 'react-plx';
 
-import Header from 'components/common/Header';
-import ScrollArrow from 'components/common/ScrollArrow';
-import WordCycler from 'components/home/WordCycler';
+import AboutSection from 'components/home/AboutSection';
 import Explosion from 'components/transitions/Explosion';
 
 import 'styles/home/Home.scss';
 
-import { Page } from 'utils/types';
-
 const Home = (): JSX.Element => (
   <div>
-    <Plx
-      parallaxData={
-        [
-          {
-            start: 'self',
-            duration: '50vh',
-            properties: [
-              {
-                startValue: 1,
-                endValue: 0,
-                property: 'opacity',
-              },
-            ],
-          },
-        ]
-      }
-    >
-      <div className="firstSection">
-        <Header page={Page.About} />
-        <div className="about">
-          <h2>Hi there! I&apos;m Roy, a</h2>
-          <div className="descriptor">
-            <WordCycler words={['student', 'developer', 'musician']} />
-          </div>
-          <p>currently attending Duke University.</p>
-          <p>I&apos;ll be graduating in Spring 2022 with a B.S. in Computer Science!</p>
-        </div>
-        <ScrollArrow />
-      </div>
-    </Plx>
+    <AboutSection />
     <Explosion />
     <div
       className="secondSection"
